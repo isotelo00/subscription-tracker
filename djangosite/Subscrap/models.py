7c160bd1afa719088e83d5f8c8c51e2cf185251f
GabrielSuzuki <60160196+GabrielSuzuki@users.noreply.github.com>
2021-10-29 18:12:47 -0700

    started making the database models

--- djangosite/Subscrap/models.py
+++ djangosite/Subscrap/models.py
@@ -17,3 +17,29 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class SubscriptionList(models.Model):
+    # name of person
+    name = models.CharField(max_length=200)
+    # name of List
+    listName = models.CharField(max_length=200)
+    # id of person
+    id = models.IntegerField(max_length=100)
+
+    def __str__(self):
+        return self.listName
+
+
+class SubscriptionItem(models.Model):
+    SubscriptionList = models.ForeignKey(
+        SubscriptionList, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    id = models.IntegerField(max_length=100)
+    startDate = models.DateTimeField()
+    expirationDate = models.DateTimeField()
+    postitionInList = models.IntegerField()
+    muted = models.BooleanField()
+
+    def __str__(self):
+        return self.name
